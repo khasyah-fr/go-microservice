c2d0430e72126fb28042d90e210bbec279ea3a50
khasyahfr <bytenauts@gmail.com>
2021-03-09 23:45:20 +0700

    Add PUT product endpoint

--- handlers/products.go
+++ handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"gomicroservice/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -28,6 +30,36 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPut {
+		reg := regexp.MustCompile("/([0-9]+)")
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			http.Error(rw, "Fail to receive id", http.StatusInternalServerError)
+			return
+		}
+
+		p.l.Println("Got id", id)
+
+		p.updateProducts(id, rw, r)
+
+		return
+	}
+
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -44,10 +76,35 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST Product")
 
-	prod := data.Product{}
+	prod := &data.Product{}
 	err := prod.FromJSON(r.Body)
 
 	if err != nil {
 		http.Error(rw, "Unable to unmarshal JSON", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal JSON", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Fail to update product", http.StatusInternalServerError)
+		return
 	}
 }
